089987e5c2a841c57eb91ccd79c5d95634d7851f
Elder Louzada <elder_louzada@outlook.com>
2019-11-02 14:09:03 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -1,16 +1,3 @@
-// const toggleCompleted = (completed, id) => {
-//     const formData = new FormData()
-//     formData.append("completed", !completed)
-//     fetch("/api/todos/" + id, {
-//         method: "PUT",
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         },
-//         body: formData
-//     }).then(data =>data.json())
-//     .then(data => {console.log(data)})
-// }
-
 const addNewTodo = e => {
     if (e.keyCode === 13) {
         $.post("/api/todos/", { name: e.target.value })
@@ -18,28 +5,58 @@ const addNewTodo = e => {
                 document.querySelector("#add-todo").value = ""
                 addTodos([data])
             })
-            .catch(err => {console.log(err)})
+            .catch(err => { console.log(err) })
     }
 
 }
 
 const addTodos = objs => objs.forEach(obj => {
-    const li = $(`<li aria-id='${obj._id}' class='${obj.completed ? 'done' : ''}'>${obj.name}<button class='delete'>X</button></li>`)
+    const li = $(`<li completed='${obj.completed}' class='${obj.completed ? 'done' : ''}'><span>${obj.name}</span><button class='delete'>X</button></li>`)
     $(".todos").append(li)
+    li.data("id", obj._id)
+    li.data("completed", obj.completed)
 })
 
-$(document).ready(() => {
-    $.getJSON("/api/todos")
-        .then(addTodos)
+const deleteTodo = e => {
+    e.stopPropagation()
+    const parent = $(e.target).parent()
+    const id = parent.data("id")
+
+    parent.remove()
+
+    $.ajax({
+        url: `/api/todos/${id}`,
+        method: "DELETE"
+    })
+        .then(res => { console.log(res) })
+        .catch(err => { console.log(err) })
+}
+
+const updateTodo = e => {
+    const target = $(e.target).prop("tagName") === "SPAN" ? $(e.target).parent() : $(e.target)
+    const id = target.data("id")
+    const completedStatus = target.data("completed")
     
+    $.ajax({
+        method: "PUT",
+        url: `/api/todos/${id}`,
+        data: {
+            completed: !completedStatus
+        }
+    })
+        .then(() => { target.toggleClass("done"); target.data("completed", !completedStatus) })
+        .catch(err => { console.log(err) })
+
+}
+
+
+$(document).ready(() => {
+    $.getJSON("/api/todos/")
+        .then(addTodos)
+
     $("#add-todo").on("keyup", addNewTodo)
 
-    $(".todos").on('click', 'button' , () => {
-        $(this).parent().remove()
-        console.log($(this).parent().text())
-        // $.ajax({
-        //     method: "DELETE",
-        //     url: "/api/todos/" + $(this).parent().data("id")
-        // }).then(data => {console.log(data)})
-    })
+    $(".todos").on('click', 'button', deleteTodo)
+
+    $(".todos").on('click', 'li', updateTodo)
 })
